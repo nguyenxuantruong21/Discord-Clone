0dc1f035be2d4e4224a37e448d85424c48231f0d
truongnguyen <xuantruong200101@gmail.com>
2024-07-30 14:24:09 +0700

    navigation sidebar

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,37 @@
+"use client";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import React from "react";
+
+interface ActionTooltipProps {
+  lable: string;
+  children: React.ReactNode;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
+}
+
+const ActionTooltip = ({
+  lable,
+  children,
+  side,
+  align,
+}: ActionTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {lable.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default ActionTooltip;
---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import ActionTooltip from "@/components/action-tooltip";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+
+interface NavigationItemProps {
+  id: string;
+  name: string;
+  imageUrl: string;
+}
+const NavigationItem = ({ id, name, imageUrl }: NavigationItemProps) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const onClick = () => {
+    router.push(`/servers/${id}`);
+  };
+
+  return (
+    <ActionTooltip side="right" align="center" lable={name}>
+      <button className="group relative flex items-center" onClick={onClick}>
+        <div
+          className={cn(
+            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[36px]" : "h-[8px]"
+          )}
+        />
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params?.serverId === id &&
+              "bg-primary/10 text-primary rounded-[16px]"
+          )}
+        >
+          <Image fill src={imageUrl} alt="channel" />
+        </div>
+      </button>
+    </ActionTooltip>
+  );
+};
+
+export default NavigationItem;
