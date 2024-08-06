0f7b1a87476b2f9e7490464b22ff3361f4c3f803
truongnguyen <xuantruong200101@gmail.com>
2024-08-06 10:06:50 +0700

    chat input

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import NavigationSidebar from "@/components/navigation/navigation-sidebar";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full">
+    <div className="h-full mt-[-24px]">
       <div className="md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
         <NavigationSidebar />
       </div>
